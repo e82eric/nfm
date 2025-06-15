369fd811bef1805364669a991018533034b7bc57
Eric Nelson <e82.eric@gmail.com>
2025-06-15 00:32:07 -0700

    Fix to not hardcode history path

--- PowershellHistoryReader/Program.cs
+++ PowershellHistoryReader/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System.Threading.Channels;
 using nfm.menu;
 
+var historyPath =
+    (args.Length > 0 ? args[0] : null)
+    ?? Path.Combine(
+           Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+           "Microsoft", "Windows", "PowerShell", "PSReadLine",
+           "ConsoleHost_history.txt");
+
+if (!File.Exists(historyPath))
+{
+    Console.Error.WriteLine($"History file not found: {historyPath}");
+    return;
+}
+
 var channel = Channel.CreateUnbounded<object>();
-await ReverseFileReader.Read(
-    @"C:\Users\eric\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt",
-    channel.Writer);
+_ = Task.Run(() => ReverseFileReader.Read(historyPath, channel.Writer));
 
 await foreach (var item in channel.Reader.ReadAllAsync())
 {
