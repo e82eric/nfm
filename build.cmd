7e9e5e90dfccb86651fee77e6ba94a974654808f
Eric Nelson <e82eric@gmail.com>
2025-06-30 23:18:34 -0700

    Diable parallel build to avoid file lock errors

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 REM rd /s /q bin\cli
-dotnet publish Cli\Cli.csproj -c Release -r win-x64 -o "bin"
-dotnet publish PowershellHistoryReader\PowershellHistoryReader.csproj -c Release -r win-x64 -o "bin"
-dotnet publish LibNfm\libnfm.csproj -c Release -r win-x64 -o "bin\libnfm"
+dotnet publish Cli\Cli.csproj -c Release -r win-x64 -o "bin" --disable-parallel
+dotnet publish PowershellHistoryReader\PowershellHistoryReader.csproj -c Release -r win-x64 -o "bin" --disable-parallel
+dotnet publish LibNfm\libnfm.csproj -c Release -r win-x64 -o "bin\libnfm" --disable-parallel
